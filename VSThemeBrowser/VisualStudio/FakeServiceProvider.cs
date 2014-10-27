fecb98defa7726decb880e9058e792c06aee31b6
SLaks <Dev@SLaks.net>
2014-10-27 08:55:54 -0400

    Begin SQM session

--- VSThemeBrowser/VisualStudio/FakeServiceProvider.cs
+++ VSThemeBrowser/VisualStudio/FakeServiceProvider.cs
@@ -54,8 +54,12 @@ namespace VSThemeBrowser.VisualStudio {
 
 			if (VsLoader.RoslynAssemblyPath != null) {
 
+				var sqm = RoslynSqmLoader.TryGetSqmService();
 				// Key is SVsLog, in a private PIA
-				sp.serviceInstances.Add(new Guid("2508FDF0-EF80-4366-878E-C9F024B8D981"), RoslynSqmLoader.TryGetSqmService());
+				sp.serviceInstances.Add(new Guid("2508FDF0-EF80-4366-878E-C9F024B8D981"), sqm);
+
+				uint sessionHandle;
+				sqm.BeginSession(775u, false, out sessionHandle);
 			}
 
 			ServiceProviderRegistration.CreateFromSetSite(sp);
@@ -74,7 +78,7 @@ namespace VSThemeBrowser.VisualStudio {
 		}
 		static class RoslynSqmLoader {
 			//static readonly Type queryServiceDelegateType = Type.GetType("Microsoft.VisualStudio.Shell.Interop.SqmServiceProvider+QueryServiceDelegate, Microsoft.CodeAnalysis.Desktop");
-			private delegate bool QueryServiceDelegate([In]ref Guid rsid, [In]ref Guid riid, [Out][MarshalAs(UnmanagedType.IUnknown)]out object vssqm);
+			private delegate bool QueryServiceDelegate([In]ref Guid rsid, [In]ref Guid riid, [Out]out IVsSqmMulti vssqm);
 			private static Lazy<QueryServiceDelegate> queryService = new Lazy<QueryServiceDelegate>(TryGetSqmServiceDelegate, true);
 
 			[DllImport("kernel32.dll")]
@@ -104,8 +108,8 @@ namespace VSThemeBrowser.VisualStudio {
 				}
 				return null;
 			}
-			public static object TryGetSqmService() {
-				object result = null;
+			public static IVsSqmMulti TryGetSqmService() {
+				IVsSqmMulti result = null;
 				Guid rsid = new Guid("2508FDF0-EF80-4366-878E-C9F024B8D981");
 				Guid riid = new Guid("B17A7D4A-C1A3-45A2-B916-826C3ABA067E");
 				if (queryService.Value != null) {
@@ -435,6 +439,162 @@ namespace Microsoft.VisualStudio.Feedback.Interop {
 			[MethodImpl(MethodImplOptions.InternalCall)]
 			get;
 		}
+	}
+}
+namespace Microsoft.VisualStudio.Shell.Interop {
+	// Copied from http://source.roslyn.codeplex.com/Microsoft.CodeAnalysis.Desktop/IVsSQM.cs
+	[ComImport()]
+	[ComVisible(false)]
+	[Guid("B17A7D4A-C1A3-45A2-B916-826C3ABA067E")]
+	[InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+	internal interface IVsSqmMulti {
+		[return: MarshalAs(UnmanagedType.VariantBool)]
+		bool GetOptInStatus();
+		void UnloadSessions(
+			);
+		void EndAllSessionsAndAbortUploads(
+			);
+		void BeginSession(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionType,
+			[In, MarshalAs(UnmanagedType.VariantBool)] System.Boolean alwaysSend,
+			[Out, MarshalAs(UnmanagedType.U4)] out System.UInt32 sessionHandle
+			);
+		void EndSession(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle
+			);
+		void RegisterSessionHandle(
+			[In] ref Guid sessionIdentifier,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dwSessionHandle
+		);
+		[return: MarshalAs(UnmanagedType.U4)]
+		int GetSessionHandleByIdentifier(
+			[In] ref Guid sessionIdentifier
+		 );
+		void GetSessionStartTime(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[Out] out System.Runtime.InteropServices.ComTypes.FILETIME time
+			);
+		Guid GetGlobalSessionGuid();
+		[return: MarshalAs(UnmanagedType.U4)]
+		int GetGlobalSessionHandle();
+		void SetGlobalSessionGuid(
+			[In] ref Guid pguidSessionGuid
+			);
+		void GetFlags(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[Out, MarshalAs(UnmanagedType.U4)] out System.UInt32 flags
+			);
+		void SetFlags(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 flags
+			);
+		void ClearFlags(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 flags
+			);
+		void SetDatapoint(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void SetBoolDatapoint(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 fValue
+			);
+		void SetStringDatapoint(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.BStr)] string strValue
+			);
+		void SetDatapointBits(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void IncrementDatapoint(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+
+		void SetDatapointIfMax(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void SetDatapointIfMin(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void AddToDatapointAverage(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void StartDatapointTimer(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID
+			);
+		void RecordDatapointTimer(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID
+			);
+		void AccumulateDatapointTimer(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID
+			);
+		void AddTimerToDatapointAverage(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID
+			);
+		void AddItemToStream(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void AddArrayToStream(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4, SizeParamIndex = 2)] System.UInt32[] data,
+			[In, MarshalAs(UnmanagedType.I4)] int count
+		);
+		void AddToStreamDWord(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 cTuple,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void AddToStreamString(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 cTuple,
+			[In, MarshalAs(UnmanagedType.BStr)] string strValue
+			);
+		void RecordCmdData(
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 sessionHandle,
+			[In] ref Guid pguidCmdGroup,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 dataPointID,
+			[In, MarshalAs(UnmanagedType.U4)] System.UInt32 value
+			);
+		void GetHashOfGuid(
+			[In] ref Guid hashGuid,
+			[Out, MarshalAs(UnmanagedType.U4)] out System.UInt32 resultantHash
+			);
+		void GetHashOfString(
+			[In, MarshalAs(UnmanagedType.BStr)] string hashString,
+			[Out, MarshalAs(UnmanagedType.U4)] out System.UInt32 resultantHash
+			);
+		void SetProperty(
+			 [In, MarshalAs(UnmanagedType.U4)] System.UInt32 propid,
+			 [In] ref Guid varKey,
+			 [In] object varValue
+			);
+		void Get64BitHashOfString(
+			[In, MarshalAs(UnmanagedType.BStr)] string hashString,
+			[Out, MarshalAs(UnmanagedType.U8)] out System.UInt64 resultantHash
+			);
 	}
 }
 namespace Microsoft.Internal.VisualStudio.Shell.Interop {
