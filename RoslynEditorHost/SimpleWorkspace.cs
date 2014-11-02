85e2a1d1a23e63719960a8cad8d1bc9f73004a1c
Schabse Laks <Dev@SLaks.net>
2014-11-02 07:26:20 -0500

    Make code actions update text buffer

--- RoslynEditorHost/SimpleWorkspace.cs
+++ RoslynEditorHost/SimpleWorkspace.cs
@@ -13,6 +13,7 @@ namespace RoslynEditorHost {
 	class SimpleWorkspace : Workspace {
 		static readonly Type IWorkCoordinatorRegistrationService = Type.GetType("Microsoft.CodeAnalysis.SolutionCrawler.IWorkCoordinatorRegistrationService, Microsoft.CodeAnalysis.Features");
 
+		readonly Dictionary<DocumentId, ITextBuffer> documentBuffers = new Dictionary<DocumentId, ITextBuffer>();
 		public SimpleWorkspace(HostServices host) : base(host, "Host") {
 			var wcrService = typeof(HostWorkspaceServices)
 				.GetMethod("GetService")
@@ -28,6 +29,7 @@ namespace RoslynEditorHost {
 		}
 		public Document CreateDocument(ProjectId projectId, ITextBuffer buffer) {
 			var id = DocumentId.CreateNewId(projectId);
+			documentBuffers.Add(id, buffer);
 
 			var docInfo = DocumentInfo.Create(id, "Sample Document",
 				loader: TextLoader.From(buffer.AsTextContainer(), VersionStamp.Create()),
@@ -41,6 +43,11 @@ namespace RoslynEditorHost {
 
 		protected override void AddMetadataReference(ProjectId projectId, MetadataReference metadataReference) {
 			OnMetadataReferenceAdded(projectId, metadataReference);
+		}
+		protected override void ChangedDocumentText(DocumentId id, SourceText text) {
+			OnDocumentTextChanged(id, text, PreservationMode.PreserveValue);
+			var buffer = documentBuffers[id];
+			buffer.Replace(new Span(0, buffer.CurrentSnapshot.Length), text.ToString());
 		}
 	}
 }
