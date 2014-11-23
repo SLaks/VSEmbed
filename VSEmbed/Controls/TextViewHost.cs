b249b6c77f56c60a88fa44efcafb2e239d36f84a
Schabse Laks <Dev@SLaks.net>
2014-11-22 22:37:50 -0500

    Make TextViewHost use DependencyProperties for bindability

--- VSEmbed/Controls/TextViewHost.cs
+++ VSEmbed/Controls/TextViewHost.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.VisualStudio.Text;
@@ -26,23 +27,43 @@ namespace VSEmbed.Controls {
 				editorFactory.AllPredefinedRoles
 			);
 			Content = editorFactory.CreateTextViewHost(TextView, false).HostControl;
+
+			TextView.TextBuffer.ContentTypeChanged += (s, e) => ContentType = TextView.TextBuffer.ContentType.DisplayName;
+			TextView.TextBuffer.Changed += (s, e) => Text = TextView.TextSnapshot.GetText();
+		}
+
+
+		public string ContentType {
+			get { return (string)GetValue(ContentTypeProperty); }
+			set { SetValue(ContentTypeProperty, value); }
+		}
+
+		public static readonly DependencyProperty ContentTypeProperty =
+			DependencyProperty.Register("ContentType", typeof(string), typeof(TextViewHost), new PropertyMetadata(ContentType_Changed));
+		private static void ContentType_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			var instance = (TextViewHost)d;
+			var contentTypeRegistry = VsServiceProvider.Instance.ComponentModel.GetService<IContentTypeRegistryService>();
+
+			var contentType =
+				contentTypeRegistry.GetContentType(e.NewValue as string ?? "text")
+			 ?? contentTypeRegistry.GetContentType("text");
+
+			instance.TextView.TextBuffer.ChangeContentType(contentType, null);
 		}
 
 		public string Text {
-			get { return TextView.TextSnapshot.GetText(); }
-			set {
-				TextView.TextBuffer.Replace(new Span(0, TextView.TextSnapshot.Length), value);
-			}
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
 		}
 
-		public string ContentType {
-			get { return TextView.TextBuffer.ContentType.TypeName; }
-			set {
-				var contentType = VsServiceProvider.Instance.ComponentModel
-					.GetService<IContentTypeRegistryService>()
-					.GetContentType(value);
-				TextView.TextBuffer.ChangeContentType(contentType, null);
-			}
+		// Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty TextProperty =
+			DependencyProperty.Register("Text", typeof(string), typeof(TextViewHost), new PropertyMetadata(Text_Changed));
+		private static void Text_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			var instance = (TextViewHost)d;
+			var value = e.NewValue as string;
+			if (value != instance.Text)
+				instance.TextView.TextBuffer.Replace(new Span(0, instance.TextView.TextSnapshot.Length), value);
 		}
 	}
 }
