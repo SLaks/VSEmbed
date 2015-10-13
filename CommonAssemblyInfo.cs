8851ce667664dd54f79d820965377ee5c27a73d8
SLaks <Dev@SLaks.net>
2015-10-13 19:11:29 -0400

    Centralize assembly version

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("SLaks")]
+[assembly: AssemblyProduct("VSEmbed")]
+[assembly: AssemblyCopyright("Copyright © SLaks 2015")]
+
+[assembly: AssemblyVersion(ProductVersion.Current)]
+[assembly: AssemblyFileVersion(ProductVersion.Current)]
+[assembly: AssemblyInformationalVersion(ProductVersion.Current)]
+static class ProductVersion { public const string Current = "1.2.0"; }
--- VSEmbed/Properties/AssemblyInfo.cs
+++ VSEmbed/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("VSEmbed")]
 [assembly: AssemblyDescription("Embeds Visual Studio's editor and theming system in standalone projects.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SLaks")]
-[assembly: AssemblyProduct("VSEmbed")]
-[assembly: AssemblyCopyright("Copyright © SLaks 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,20 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93bbeb44-6849-423e-a5c1-440558661b5c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion(ProductVersion.Current)]
-[assembly: AssemblyFileVersion(ProductVersion.Current)]
-[assembly: AssemblyInformationalVersion(ProductVersion.Current)]
-static class ProductVersion { public const string Current = "1.2.0"; }
-
